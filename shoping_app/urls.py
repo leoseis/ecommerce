24d635874a23844ab5c0e21f97a2db1f96fd719e
leoseis <eboscrib2@yahoo.com>
2025-08-13 16:36:00 +0100

    add a product detail function

--- shoping_app/urls.py
+++ shoping_app/urls.py
@@ -3,4 +3,5 @@ from .import views
 
 urlpatterns =[
 path("products", views.products, name="products"),
+path("product_detail/<slug:slug>", views.product_detail, name="product_detail")
 ]
--- shoping_app/views.py
+++ shoping_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from .serializers import ProductSerializer
+from .serializers import DetailedProductSerializer, ProductSerializer
 from rest_framework.response import Response
 from .models import Product
 
@@ -11,5 +11,14 @@ def products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+
+
+@api_view(["GET"])
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    serializer = DetailedProductSerializer(product)
+    return Response(serializer.data)
+
 
 # Create your views here.
